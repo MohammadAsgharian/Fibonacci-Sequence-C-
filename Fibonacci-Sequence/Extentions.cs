b4f2410bb06bdafd2f61bf70554cdb5c27a58a14
MohammadAsgharian <m.asgharian@gmail.com>
2023-01-21 18:16:02 +0330

    Fibonacci-Sequence

---
+++ Fibonacci-Sequence/Extentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    public static class CustomExtentions
+    {
+        public static IEnumerable<int> AsSequence(this Range range)
+        {
+            for (var i = range.Start.Value; i <= range.End.Value; i++)
+                yield return i;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> seq, Action<T> action)
+        {
+            foreach (var item in seq) action(item);
+        }
+    }
+}
---
+++ Fibonacci-Sequence/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fibonacci_Sequence;
+
+var series = Fibonacci(10);
+Console.WriteLine(string.Join(",", series));
+
+IReadOnlyList<int> Fibonacci(int num)
+{
+    var series = new List<int> { 0, 1 };
+
+    (2..num)
+        .AsSequence()
+        .Select(it => series[it - 1] + series[it - 2])
+        .ForEach(it => series.Add(it));
+
+    return series.AsReadOnly();
+}
+
+
